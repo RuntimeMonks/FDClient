6045c9ead89d589b1345b1f74fed44d27eaacbf1
akshay <akki24.akshay@gmail.com>
2019-11-20 10:15:31 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        redirectTo: '/login',
+        pathMatch: 'full'
+      },
+      {path: 'login',
+      component: LoginComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
