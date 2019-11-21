64178ea931eb0e1d65ce195b8296f17eacc677de
Anand Lok <imanandlok@gmail.com>
2019-11-21 11:34:27 +0530

    21MB

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 const routes: Routes = [
   { path:'home',component:HomePageComponent },
   { path:'login' ,component:LoginComponent},
   { path:'', redirectTo: '/home', pathMatch: 'full' },
+  { path:'sidebar' , component:SidebarComponent},
 ];
 
 @NgModule({
