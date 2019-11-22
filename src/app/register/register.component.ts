c74a14502627bf3ca0f48557a58dce653be94c96
sonalj97 <57439140+sonalj97@users.noreply.github.com>
2019-11-22 18:53:13 +0530

    Event Crud1

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,31 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {RegserviceService} from '../regservice.service';
 import {  registerLocaleData } from '@angular/common';
 import{FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+selector: 'app-register',
+templateUrl: './register.component.html',
+styleUrls: ['./register.component.css']
 })
 
 export class RegisterComponent implements OnInit {
-  public form: FormGroup;
- regdata={};
+public form: FormGroup;
+regdata={};
+err=[];
+constructor(private  _msg1:RegserviceService)
+{
 
-  constructor(private  _msg1:RegserviceService)
-   {
-    
-   }
+}
 
-  ngOnInit() 
-  {
-    
-    
-  }
+ngOnInit() 
+{
+
+
+}
 onsubmit1(regform:NgForm): void {
-  this._msg1.register(regform);
+this._msg1.register(regform);
 const uname=regform.value.fname
 const email=regform.value.mail
 const eid=regform.value.eid
@@ -34,17 +34,17 @@ const password=regform.value.pwd
 
 // console.log(uname + email + eid) 
 
- 
-  if(regform.value.pwd !== regform.value.cpwd)
-  {
-    alert("password is not matching")
-   
-  }
-  else{
-    console.log(regform.value)
-    console.log(regform.value.fname)
-  }
-  
+
+if(regform.value.pwd !== regform.value.cpwd)
+{
+// alert("password is not matching")
+this.err.push("password is not matching");
+}
+else{
+console.log(regform.value)
+console.log(regform.value.fname)
+}
+
 }
 
 
