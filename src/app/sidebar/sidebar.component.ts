d3e90fb7a74b3b4a6c83e2a373f163980267a676
Anand Lok <imanandlok@gmail.com>
2019-11-25 20:29:57 +0530

    25nb

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private routes:Router) { }
 
   showsidebar : boolean = true;
   showbar : boolean = false;
@@ -24,6 +25,7 @@ export class SidebarComponent implements OnInit {
 
   login(){
     this.logButton = false;
+    this.routes.navigate(["/login"]);
   }
 
 }
